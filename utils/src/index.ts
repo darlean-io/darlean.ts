50299a973725d8fdb8cd29e66385a94a0f57564b
Theo van der Donk <t.v.d.donk@choofy.nl>
2023-01-09 20:54:10 +0100

    Removed DarleanError type

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Set of utility functions and classes have no specific relation with virtual actors.
+ * 
+ * This package can be used stand-alone without dependencies on {@link @darlean/base}, {@link @darlean/core} or
+ * any other darlean package.
+ * 
+ * @packageDocumentation
+ */
 export * from './formatting';
 export * from './logging';
 export * from './parallel';
@@ -7,4 +15,3 @@ export * from './time';
 export * from './timeimpl';
 export * from './tracing';
 export * from './util';
-export * from './errors';
